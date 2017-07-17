a25a35ba8ace2bacecff6b512fd5c32b02a18f3d
Kirill Gusev <kirikg221@mail.ru>
2017-07-17 20:55:26 +0300

    3 ok

--- 3.js
+++ 3.js
@@ -12,10 +12,13 @@ function chunkArrayInGroups(arr, size) {
   let j = 0;
   const remnant = arr.length % size;
 
+  if (!(size > 0)) {
+    return -1;
+  }
+
   for (let i = 0; i < arr.length; i += size, j++) {
     buff = arr.slice(i, i+size);
     result[j] = buff;
-    console.log(buff);
   }
 
   return result;
