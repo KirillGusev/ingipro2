b6ebd9efc671b90f446439bef03133244d01a11c
Kirill Gusev <kirikg221@mail.ru>
2017-07-17 19:13:13 +0300

    3 ok

---
+++ 3.js
@@ -0,0 +1,24 @@
+/*Написать функцию, котоая разбивает переданный массив arr на группы длиной size.
+ function chunkArrayInGroups(arr, size) {
+ // your code
+ }
+ chunkArrayInGroups(["a", "b", "c", "d"], 2); // [["a", "b"], ["c", "d"]]*/
+
+'use strict';
+
+function chunkArrayInGroups(arr, size) {
+  let result = [];
+  let buff = [];
+  let j = 0;
+  const remnant = arr.length % size;
+
+  for (let i = 0; i < arr.length; i += size, j++) {
+    buff = arr.slice(i, i+size);
+    result[j] = buff;
+    console.log(buff);
+  }
+
+  return result;
+}
+
+console.log(chunkArrayInGroups(["a", "b", "c", "d"], 2)); // [["a", "b"], ["c", "d"]]
