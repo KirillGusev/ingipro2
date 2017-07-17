9aa8b73bcbc0af2c4f29ad71c234a503f631724c
Kirill <Kirill>
2017-07-17 18:53:04 +0300

    1 ok

---
+++ 1.js
@@ -0,0 +1,39 @@
+/**
+ * Created by Интеграл on 17.07.2017.
+ */
+/**Написать функцию reverseString, которая будет возвращать реверсированную строку
+function reverseString(str) {
+  // your code
+} */
+'use strict';
+
+function reverseString(str) {
+  let result = "";
+
+  if (!str) {
+    return null;
+  }
+
+  for (let i = 0; i < str.length; i++) {
+    result += str[str.length - 1 - i];
+  }
+
+  return result;
+}
+
+//второй вариант
+
+function reverseString2(str) {
+  let arr = [];
+  let result = "";
+
+  if (!str) {
+    return null;
+  }
+
+  arr = str.split('');
+  arr.reverse();
+  result = arr.join('');
+
+  return result;
+}
