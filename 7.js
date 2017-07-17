8c34598b346d5806d5929b6cd5324e8b8607a8d2
Kirill Gusev <kirikg221@mail.ru>
2017-07-18 00:04:15 +0300

    7 ok

---
+++ 7.js
@@ -0,0 +1,73 @@
+/**
+ * Created by Интеграл on 18.07.2017.
+ */
+/**
+ * Created by Интеграл on 17.07.2017.
+ */
+/*
+ Напишите фукнцию сортировки sortLibrary массива объектов.
+ Аргументы функции:
+ library - массив объектов
+ field_name - ключ, по которому нужно отсортировать
+ reverse - порядок сортировки. Если true - по убыванию, false - по возрастанию
+ initial - функция преобразования значения, соответствующего ключу. Если передана, сортировка будет по значению
+ initial(value)*/
+
+function sortLibrary(library, field_name, reverse, initial = x => x) {
+  library.sort(sortFunction);
+
+  function sortFunction(a,b) {
+    if (initial(a[field_name]) < initial(b[field_name]))
+    {
+      return -1;
+    }
+    if (initial(a[field_name]) > initial(b[field_name]))
+    {
+      return 1;
+    }
+    return 0;
+  }
+
+  if (reverse === true){
+    library.reverse();
+  }
+
+  return library;
+}
+const library = [
+  {
+    title: 'Bill Gates',
+    author: 'The Road Ahead',
+    libraryID: 1254
+  },
+  {
+    title: 'Steve Jobs',
+    author: 'Walter Isaacson',
+    libraryID: 4264
+  },
+  {
+    title: 'Mockingjay: The Final Book of The Hunger Games',
+    author: 'Suzanne Collins',
+    libraryID: 3245
+  }
+];
+console.log(sortLibrary(library, 'libraryID', true, parseInt));
+/*
+ [
+ {
+ title: 'Steve Jobs',
+ author: 'Walter Isaacson',
+ libraryID: 4264
+ },
+ {
+ title: 'Mockingjay: The Final Book of The Hunger Games',
+ author: 'Suzanne Collins',
+ libraryID: 3245
+ },
+ {
+ title: 'Bill Gates',
+ author: 'The Road Ahead',
+ libraryID: 1254
+ }
+ ]
+ */
