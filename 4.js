afafc4687f24fdfe19eee457794ebb2e6125f88a
KirillGusev <kirikg221@mail.ru>
2017-07-18 11:43:54 +0300

    Update 4.js

--- 4.js
+++ 4.js
@@ -1,29 +1,31 @@
 /*
-Написать функцию, которая возвращает сумму указанных чисел и всех чисел между ними. Шаг равен 1.
-Первое число необязательно наименьшее
-function sumAll(arr) {
-  // your code
-}
-sumAll([1, 4]); // 10 (1+2+3+4)
-sumAll([10, 5]) // 45 (10+9+8+7+6+5) */
+ Написать функцию, которая возвращает сумму указанных чисел и всех чисел между ними. Шаг равен 1.
+ Первое число необязательно наименьшее
+ function sumAll(arr) {
+ // your code
+ }
+ sumAll([1, 4]); // 10 (1+2+3+4)
+ sumAll([10, 5]) // 45 (10+9+8+7+6+5) */
 'use strict';
+
+function isNumber(n) {
+  return !isNaN(parseFloat(n)) && isFinite(n);
+}
 
 function sumAll(arr) {
   let result = 0;
-  let low;
-  let higher;
 
-  if (!(typeof arr[0] === "number") || !(typeof arr[1] === "number")) {
+  if (!isNumber(arr[0]) || !isNumber(arr[1])) {
     return -1;
   }
 
   if (arr[0] > arr[1]) {
-    let a =arr[0];
+    let a = arr[0];
     arr[0] = arr[1];
     arr[1] = a;
   }
 
-  for (let i = low; i <= higher; i++) {
+  for (let i = arr[0]; i <= arr[1]; i++) {
     result += i;
   }
 
@@ -31,4 +33,4 @@ function sumAll(arr) {
 }
 
 sumAll([1, 4]); // 10 (1+2+3+4)
-sumAll([10, 5]) // 45 (10+9+8+7+6+5)
+sumAll([10, 5]); // 45 (10+9+8+7+6+5)
